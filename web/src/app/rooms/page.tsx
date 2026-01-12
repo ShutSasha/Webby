f2d9093e3854c416baa5f95ad3526fa40bd556c4
ShutSasha <sanyashut159@gmail.com>
2026-01-12 13:41:20 +0200

    feat(web): added rooms' genres

--- web/src/app/rooms/page.tsx
+++ web/src/app/rooms/page.tsx
@@ -1,11 +1,23 @@
+import CategoryToggle from '@/ui/components/common/GenreToggle'
 import MediaHeader from '@/ui/components/common/MediaHeader'
 import MainLayout from '@/ui/components/MainLayout'
 
 export default function RoomPage() {
   return (
     <MainLayout>
-      <div className="w-full bg-neutral-900 rounded-[20px] p-5 gap-4 box-border">
+      <div className="flex flex-col w-full bg-neutral-900 rounded-[20px] p-5 gap-4 box-border">
         <MediaHeader searchPlaceholder="Search rooms..." actionLabel="Create a room" />
+
+        <div className="flex justify-between">
+          <div>
+            <div className="flex gap-3 items-center">
+              <span className="w-10 h-0.5 bg-emerald-500 rounded-full" />
+              <p className="text-emerald-500 font-black text-[10px] leading-3.5 uppercase">live now</p>
+            </div>
+            <h2 className="font-black text-[48px] leading-14 text-emerald-500 uppercase">Rooms</h2>
+          </div>
+          <CategoryToggle />
+        </div>
       </div>
     </MainLayout>
   )
