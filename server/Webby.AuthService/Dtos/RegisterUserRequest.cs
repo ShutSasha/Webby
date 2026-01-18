b11c929a9632c1bbf24aba3da204de2efec2fd3f
Artem <118220003+guntersteam@users.noreply.github.com>
2026-01-18 19:48:28 +0200

    hotfix: fix registration endpoint validation issue

--- server/Webby.AuthService/Dtos/RegisterUserRequest.cs
+++ server/Webby.AuthService/Dtos/RegisterUserRequest.cs
@@ -9,10 +9,10 @@ public class RegisterUserRequest
    public string Email { get; set; }
    
    [Required]
-   [Range(3, 25, ErrorMessage = "{0} must be between {1} and {2} characters length.")]
+   [StringLength(25, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters length.")]
    public string Username { get; set; }
    
    [Required]
-   [Range(3, 25, ErrorMessage = "{0} must be between {1} and {2} characters length.")]
+   [StringLength(25, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters length.")]
    public string Password { get; set; }
 }
