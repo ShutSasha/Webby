acddf52cdc87897b8f00be35ced82e95290a25b2
Artem <118220003+guntersteam@users.noreply.github.com>
2026-01-18 13:44:21 +0200

    feat: connect routes into getaway

--- server/Webby.ApiGetaway/Program.cs
+++ server/Webby.ApiGetaway/Program.cs
@@ -1,13 +1,38 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Webby.ApiGetaway.Extensions;
 
-builder.Services.AddOpenApi();
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddOpenApi();
+services.AddCorsPolicy("AllowWebOrigin");
+
+configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+services.AddOcelot(configuration);
+services.AddEndpointsApiExplorer();
+
+services.AddSwaggerInfo();
 
 var app = builder.Build();
 
+app.UseCors("AllowWebOrigin");
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("http://localhost:5001/swagger/v1/swagger.json", "AuthService");
+        c.RoutePrefix = "";
+    });
 }
 
-app.UseHttpsRedirection();
+await app.UseOcelot();
 app.Run();
