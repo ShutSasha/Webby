ea6a637180540f5d4b11d3b9abd8a946b034c3ea
ShutSasha <sanyashut159@gmail.com>
2026-02-20 22:42:55 +0200

    feat(web): add google oauth

--- web/auth.config.ts
+++ web/auth.config.ts
@@ -1,5 +1,8 @@
 import type { NextAuthConfig } from 'next-auth'
 
+import $api from '@/app/api'
+import { GoogleAuthRes } from '@/types/auth'
+
 export const authConfig = {
   pages: {
     signIn: '/login',
@@ -10,19 +13,22 @@ export const authConfig = {
         try {
           console.log('authConfig.signIn - Google user:', user)
 
+          const { data } = await $api.post<GoogleAuthRes>('/auth/google-auth', {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+          })
+
+          console.log('GOOGLE_AUTH RESPONSE', data)
+          if (!data.success) return false
+
           user.userId = user.id as string
           user.username = user.name as string
+          user.email = user.email
           user.avatarUrl = user.image as string
-          return true
-        
-          // TODO Sync with backend
 
-          // if (backendUser) {
-          //   user.userId = backendUser.id
-          //   user.username = backendUser.username
-          //   user.avatarUrl = backendUser.image
-          //   return true
-          // }
+          return true
         } catch (error) {
           console.error('Backend sync error:', error)
           return false
