e499df147e62d347f6ef639e369b721dc69818ec
ShutSasha <sanyashut159@gmail.com>
2026-02-23 13:40:01 +0200

    feat(web): add Follows/Followers pages

--- web/src/app/profile/[id]/(details)/followers/page.tsx
+++ web/src/app/profile/[id]/(details)/followers/page.tsx
@@ -1,19 +1,30 @@
-export default async function Followers() {
+import FollowItem from '@/ui/components/modules/Profile/CommonFollow/FollowItem'
+import { FollowersToggle } from '@/ui/components/modules/Profile/CommonFollow/FollowToggle'
+import HeaderNavigation from '@/ui/components/modules/Profile/CommonFollow/HeaderNavigation'
+
+type Props = {
+  params: Promise<{ id: string }>
+}
+
+export default async function Followers({ params }: Props) {
+  const { id } = await params
+
   return (
-    <div>
-      <p>followers</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam saepe blanditiis cupiditate beatae enim minima
-        voluptas mollitia? Necessitatibus, odit distinctio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam saepe blanditiis cupiditate beatae enim minima
-        voluptas mollitia? Necessitatibus, odit distinctio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam saepe blanditiis cupiditate beatae enim minima
-        voluptas mollitia? Necessitatibus, odit distinctio.
-      </p>
+    <div className="flex flex-col gap-2">
+      {/* Header Navigation */}
+
+      <HeaderNavigation id={id} image="https://i.pinimg.com/originals/44/64/20/4464203a781eed3650f1fdd624c4d02a.jpg">
+        <FollowersToggle id={id} />
+      </HeaderNavigation>
+
+      <hr className="border-emerald-400/50 mb-1" />
+
+      {/* Grid List */}
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
+        {[...new Array(20)].map((_, index) => (
+          <FollowItem key={index} user={{ image: 'https://i.ibb.co/9fP2m4R/d5beed264cd6af09fc7a53548326c4bf.jpg' }} />
+        ))}
+      </div>
     </div>
   )
 }
--- web/src/app/profile/[id]/(details)/follows/page.tsx
+++ web/src/app/profile/[id]/(details)/follows/page.tsx
@@ -1,13 +1,27 @@
-import FollowItem from '@/ui/components/modules/Profile/FollowItem'
+import FollowItem from '@/ui/components/modules/Profile/CommonFollow/FollowItem'
+import { FollowsToggle } from '@/ui/components/modules/Profile/CommonFollow/FollowToggle'
+import HeaderNavigation from '@/ui/components/modules/Profile/CommonFollow/HeaderNavigation'
 
-export default async function Follows() {
+type Props = {
+  params: Promise<{ id: string }>
+}
+
+export default async function Follows({ params }: Props) {
+  const { id } = await params
+
   return (
-    <div className="flex flex-col gap-3">
-      <p className="text-center text-[20px] font-semibold">Follows</p>
-      <hr className="text-emerald-400" />
-      <div className="grid grid-cols-4 gap-4">
-        {[...new Array(20)].map((it, index) => (
-          <FollowItem key={index} />
+    <div className="flex flex-col gap-2">
+      {/* Header Navigation */}
+      <HeaderNavigation id={id} image="https://i.pinimg.com/originals/44/64/20/4464203a781eed3650f1fdd624c4d02a.jpg">
+        <FollowsToggle id={id} />
+      </HeaderNavigation>
+
+      <hr className="border-emerald-400/50 mb-1" />
+
+      {/* Grid List */}
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-4">
+        {[...new Array(20)].map((_, index) => (
+          <FollowItem key={index} user={{ image: 'https://i.ibb.co/ccWcyJpy/3561466ac6f721d58fd40ff2dcbaa3d6.jpg' }} />
         ))}
       </div>
     </div>
