55246176d6dc75ccf8bb94411841de64a7fc2d2b
Artem <118220003+guntersteam@users.noreply.github.com>
2026-03-14 21:49:34 +0000

    feat(server): add video service models

--- server/Webby.VideoService/Data/DbContext.cs
+++ server/Webby.VideoService/Data/DbContext.cs
@@ -1,6 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Webby.VideoService.Data.Configurations;
+using Webby.VideoService.Models;
+
 namespace Webby.VideoService.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+   public DbSet<Video> Videos { get; set; }
+   public DbSet<Tag> Tags { get; set; }
+   public DbSet<VideoTag> VideoTags { get; set; }
+
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      modelBuilder.ApplyConfiguration(new VideoConfiguration());
+      modelBuilder.ApplyConfiguration(new TagConfiguration());
+      modelBuilder.ApplyConfiguration(new VideoTagConfiguration());
+   }
 }
---
+++ server/Webby.VideoService/Models/Video.cs
@@ -0,0 +1,16 @@
+﻿namespace Webby.VideoService.Models;
+
+public class Video
+{
+   public Guid VideoId { get; set; }
+   public Guid UserId { get; set; }
+
+   public required string Name { get; set; }
+   public string? Description { get; set; }
+   public int Views { get; set; }
+   public DateTime CreatedAt { get; set; }
+   public required string VideoUrl { get; set; }
+   public required string PreviewUrl { get; set; }
+   public ICollection<VideoTag>? VideoTags { get; set; }
+   
+}
