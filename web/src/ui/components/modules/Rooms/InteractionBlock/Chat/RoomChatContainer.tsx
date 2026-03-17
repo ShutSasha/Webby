948897b676c4327a2cb12fd667e31e0aaf06923a
ShutSasha <sanyashut159@gmail.com>
2026-03-18 00:17:39 +0200

    feat(web): add room chat components

--- web/src/ui/components/modules/Rooms/InteractionBlock/Chat/RoomChatContainer.tsx
+++ web/src/ui/components/modules/Rooms/InteractionBlock/Chat/RoomChatContainer.tsx
@@ -1,11 +1,13 @@
+import RoomInteractionFooter from '../RoomInteractionFooter'
+import ChatInput from './ChatInput'
 import RoomChat from './RoomChat'
 
 export default function RoomChatContainer() {
   return (
     <div className="flex flex-col flex-1 min-h-0 gap-2">
       <RoomChat />
-      <div className="bg-red-400">input</div>
-      <div className="bg-purple-600">RoomInteractionFooter</div>
+      <ChatInput />
+      <RoomInteractionFooter />
     </div>
   )
 }
