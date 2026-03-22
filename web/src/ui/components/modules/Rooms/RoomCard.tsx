d0a6dc63b657b0601c15056cf706dfffc5d68994
ShutSasha <sanyashut159@gmail.com>
2026-03-22 17:02:44 +0200

    feat(web): redesign room card

--- web/src/ui/components/modules/Rooms/RoomCard.tsx
+++ web/src/ui/components/modules/Rooms/RoomCard.tsx
@@ -28,19 +28,9 @@ export default function RoomCard({ id }: Props) {
       <div className="absolute inset-0 z-1 bg-black/40 transition-all duration-500 group-hover:bg-black/30" />
 
       <div className="z-10 flex-1 flex flex-col justify-between">
-        {/* Card Header */}
-        <div className="flex items-center justify-between">
-          {/* Live */}
-          <div className="bg-red-600 flex items-center gap-1 px-2 py-1 rounded-lg">
-            <span className="h-1.5 w-1.5 rounded-full bg-neutral-100 block" />
-            <p className="uppercase font-black text-[9px] leading-3 text-neutral-100">live</p>
-          </div>
-
-          {/* Viewers */}
-          <div className="flex items-center gap-1 py-1 px-2 rounded-lg bg-neutral-800 overflow-hidden">
-            <UsersIcon className="text-emerald-500" />
-            <span className="text-neutral-100 font-black text-[9px] leading-3">1.2K</span>
-          </div>
+        {/* Tag */}
+        <div className="bg-neutral-900 flex items-center gap-1 px-2 py-2 rounded-lg w-fit">
+          <p className="uppercase font-bold text-[12px] leading-3 text-neutral-300 tracking-wide">anime</p>
         </div>
 
         <div className="flex flex-col gap-1">
