b776e3a54947bde9460f25c86d4b9d9d0297e8a7
Artem <118220003+guntersteam@users.noreply.github.com>
2026-03-24 11:21:54 +0000

    hotfix(server): fix playlist request model

--- server/Webby.VideoService/Dtos/Playlist/CreatePlaylistRequest.cs
+++ server/Webby.VideoService/Dtos/Playlist/CreatePlaylistRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace Webby.VideoService.Dtos.Playlist;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webby.VideoService.Dtos.Playlist;
 
 public class CreatePlaylistRequest
 {
+   [StringLength(200, MinimumLength = 3, ErrorMessage = "Field {0} must be less that {1} characters and greater than {2} characters")]
    public required string Name { get; set; }
    public required bool IsPrivate { get; set; } = false;
 }
