2c502171cf0b51dec7ee0f9444049de5e128db1e
DenysShpak0116 <shpak.denys.oleks@gmail.com>
2026-04-04 16:36:09 +0300

    fix: config file

--- scripts/generate_config.py
+++ scripts/generate_config.py
@@ -15,7 +15,7 @@ configs = [
     PROJECT_ROOT / "web/.env.temp",
     PROJECT_ROOT / "server/Webby.VideoService/appsettings.Development.json.temp",
     PROJECT_ROOT / "server/Webby.RoomService/config/config.yaml.temp",
-    PROJECT_ROOT / "server/Webby.ChatService/config/config.yaml.temp"
+    PROJECT_ROOT / "server/Webby.ChatService/config/config.yaml.temp",
     PROJECT_ROOT / "server/Webby.NotificationService/appsettings.Development.json.temp"
 ]
 
