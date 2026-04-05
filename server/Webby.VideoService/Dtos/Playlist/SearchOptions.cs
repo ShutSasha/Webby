b1852a790d0c994541df0e02cdf26e1de03b37c5
Artem <118220003+guntersteam@users.noreply.github.com>
2026-04-05 22:33:17 +0100

    fix(server): add default values in description

--- server/Webby.VideoService/Dtos/Playlist/SearchOptions.cs
+++ server/Webby.VideoService/Dtos/Playlist/SearchOptions.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +10,12 @@ public class SearchOptions
    [FromQuery(Name = "searchText")]
    public string? SearchText { get; set; }
    
+   [DefaultValue(1)]
    [Range(1,double.MaxValue, ErrorMessage ="Field {0} must be greater than {1}")]
    [FromQuery(Name = "page")]
    public int Page { get; set; } = 1;
    
+   [DefaultValue(10)]
    [Range(1,double.MaxValue, ErrorMessage ="Field {0} must be greater than {1}")]
    [FromQuery(Name = "pageSize")]
    public int PageSize { get; set; } = 10;
