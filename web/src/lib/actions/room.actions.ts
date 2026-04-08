e0374bab983e34993bb0c3bf198a4e235981454a
ShutSasha <sanyashut159@gmail.com>
2026-04-08 16:16:33 +0300

    feat(web): add base API action and hook for fetch user rooms

--- web/src/lib/actions/room.actions.ts
+++ web/src/lib/actions/room.actions.ts
@@ -3,7 +3,7 @@
 import $api from '@/lib/config/api.config'
 import { parseAxiosError, serverLog } from '@/lib/utils/general.utils'
 import { BaseServerResponse } from '@/types/general.types'
-import { GetPublicRooms } from '@/types/room.types'
+import { GetPublicRooms, GetUserRoomsResponse } from '@/types/room.types'
 
 const endpoint = '/rooms'
 
@@ -35,6 +35,38 @@ export async function getPublicRooms(
       data: null,
       success: false,
       message: `Failed to retrieve rooms from search`,
+      errors: parseAxiosError(error),
+    }
+  }
+}
+
+// TODO-ROOMS: check whether search params set correct or not
+export async function getUserRooms(
+  query: string,
+  page: number,
+  pageSize: number,
+): Promise<BaseServerResponse<GetUserRoomsResponse>> {
+  try {
+    const params = new URLSearchParams()
+
+    if (query) params.append('search', query)
+    if (page) params.append('page', page.toString())
+    if (pageSize) params.append('limit', pageSize.toString())
+
+    const queryString = params.toString()
+    // TODO-ROOMS: only url with queryString must be exist, replace it later when server is ready
+    const url = queryString ? `${endpoint}/my?${queryString}` : `${endpoint}/my`
+
+    const { data: response } = await $api.get<BaseServerResponse<GetUserRoomsResponse>>(url)
+
+    return response
+  } catch (error: unknown) {
+    serverLog('GET_USER_ROOMS_ERROR', error, true)
+
+    return {
+      data: null,
+      success: false,
+      message: `Failed to retrieve user rooms`,
       errors: parseAxiosError(error),
     }
   }
