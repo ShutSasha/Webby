e22d034fcee66d95658b76f03445f6f82ca6f10c
Artem <118220003+guntersteam@users.noreply.github.com>
2026-04-24 15:13:13 +0100

    revert(server): delete unused responses

--- server/Webby.VideoService/Helpers/Response/GlobalSearchPlaylistResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using Webby.VideoService.Dtos.Playlist;
-
-namespace Webby.VideoService.Helpers.Response;
-
-public class GlobalSearchPlaylistResponse
-{
-   public SearchSection<SearchPlaylistDto> WebbyPlaylists { get; set; } = new();
-}
