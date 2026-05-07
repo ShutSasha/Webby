35f15f255560a74d14ef2d72ee90ec2fd5388aff
ShutSasha <sanyashut159@gmail.com>
2026-05-07 23:16:20 +0300

    fix(web): player mute for YouTube

--- web/src/ui/components/modules/Player/CustomPlayer.tsx
+++ web/src/ui/components/modules/Player/CustomPlayer.tsx
@@ -71,7 +71,7 @@ export default function CustomPlayer({ videoUrl, videoId, isRoom = false }: Play
         onProgress={actions.handleProgress}
         onTimeUpdate={actions.handleTimeUpdate}
         onDurationChange={actions.handleDurationChange}
-        onVolumeChange={actions.handleReactPlayerVolumeChange}
+        onVolumeChange={(e)=>actions.handleReactPlayerVolumeChange(e, isPlatformMode)}
         onReady={actions.handleReactPlayerReady}
         onPlay={actions.handleReactPlayerPlay}
         onPause={actions.handleReactPlayerPause}
