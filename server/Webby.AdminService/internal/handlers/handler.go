ae2f16a2fd8a82d37ef6f54e3cc13d939a4190bd
DenysShpak0116 <shpak.denys.oleks@gmail.com>
2026-06-15 02:07:12 +0300

    feat: add accept complaint route

--- server/Webby.AdminService/internal/handlers/handler.go
+++ server/Webby.AdminService/internal/handlers/handler.go
@@ -3,10 +3,13 @@ package handlers
 import (
 	"context"
 	"webby/admin-service/internal/models"
+
+	"github.com/google/uuid"
 )
 
 type service interface {
 	ListComplaints(ctx context.Context, page, limit int) ([]models.Complaint, int, error)
+	AcceptComplaint(ctx context.Context, complaintID, userID uuid.UUID) error
 }
 
 type handler struct {
--- server/Webby.AdminService/internal/services/service.go
+++ server/Webby.AdminService/internal/services/service.go
@@ -4,28 +4,53 @@ import (
 	"context"
 	"fmt"
 	"webby/admin-service/internal/models"
+
+	"github.com/google/uuid"
 )
 
 type repository interface {
 	ListComplaints(ctx context.Context, offset, limit int) ([]models.Complaint, int, error)
 }
 
+type notificationSender interface {
+	SendNotificationToUser(ctx context.Context, userID, targetID uuid.UUID) error
+}
+
 type service struct {
-	repository repository
+	repository         repository
+	notificationSender notificationSender
 }
 
-func New(repository repository) *service {
-	return &service{repository}
+func New(repository repository, notificationSender notificationSender) *service {
+	return &service{
+		repository:         repository,
+		notificationSender: notificationSender,
+	}
 }
 
-func (c *service) ListComplaints(ctx context.Context, page, limit int) ([]models.Complaint, int, error) {
+func (s *service) ListComplaints(ctx context.Context, page, limit int) ([]models.Complaint, int, error) {
 	const op = "service.ListComplaints"
 
 	offset := (page - 1) * limit
-	result, total, err := c.repository.ListComplaints(ctx, offset, limit)
+	result, total, err := s.repository.ListComplaints(ctx, offset, limit)
 	if err != nil {
 		return nil, 0, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return result, total, nil
+}
+
+func (s *service) AcceptComplaint(ctx context.Context, complaintID, userID uuid.UUID) error {
+	const op = "service.AcceptComplaint"
+
+	
+
+	err := s.repository.AcceptComplaint(ctx, complaintID, userID)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	s.notificationSender.SendNotificationToUser(ctx)
+
+	return nil
 }
