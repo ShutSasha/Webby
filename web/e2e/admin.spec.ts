1adcbf9278dac410123bc8c31201c5828c5d1f24
ShutSasha <sanyashut159@gmail.com>
2026-06-22 20:17:35 +0300

    config(web): add playwright for e2e tests

---
+++ web/e2e/admin.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test'
+
+test.describe('Admin Control Panel', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/login')
+
+    await page.getByPlaceholder('Email').fill('darkvoin5@gmail.com')
+    await page.getByPlaceholder('Password').fill('qwerty123123')
+
+    await page.getByRole('button', { name: 'Log in' }).click()
+
+    await page.waitForURL('/')
+  })
+
+  test('should navigate to the admin overview page and display correct title', async ({ page }) => {
+    await page.goto('/admin')
+
+    const heading = page.getByRole('heading', { name: 'Admin Control Panel' })
+    await expect(heading).toBeVisible()
+
+    const usersStatCard = page.getByText('Total Users')
+    await expect(usersStatCard).toBeVisible()
+  })
+
+  test('should switch between tabs correctly', async ({ page }) => {
+    await page.goto('/admin')
+
+    const usersTab = page.getByRole('link', { name: 'Users Management' })
+    await usersTab.click()
+
+    await expect(page).toHaveURL(/.*\/admin\/users/)
+
+    const searchInput = page.getByPlaceholder('Search users by username...')
+    await expect(searchInput).toBeVisible()
+  })
+})
