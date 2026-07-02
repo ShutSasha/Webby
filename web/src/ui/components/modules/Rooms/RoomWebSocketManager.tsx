a8f1afb74e815d0ab04f2b83db3e014e09bb0215
ShutSasha <sanyashut159@gmail.com>
2026-07-02 15:17:50 +0300

    feat(web): apply socket event for remove user from room

--- web/src/ui/components/modules/Rooms/RoomWebSocketManager.tsx
+++ web/src/ui/components/modules/Rooms/RoomWebSocketManager.tsx
@@ -5,6 +5,7 @@ import { useSession } from 'next-auth/react'
 import { useChatSocketListeners } from '@/lib/hooks/api/room/ws/useChatSocketListeners'
 import { useEntertainmentRoomListeners } from '@/lib/hooks/api/room/ws/useEntertainmentRoomListeners'
 import { usePlayerSocketListeners } from '@/lib/hooks/api/room/ws/usePlayerSocketListeners'
+import { useRoomMemberSocketListeners } from '@/lib/hooks/api/room/ws/useRoomMemberSocketListeners'
 import { useRoomSocketConnection } from '@/lib/hooks/api/room/ws/useRoomSocketConnection'
 import { useVoteSocketListeners } from '@/lib/hooks/api/room/ws/useVoteSocketListeners'
 
@@ -21,6 +22,7 @@ export default function RoomWebSocketManager({ roomId, chatId }: Props) {
   useEntertainmentRoomListeners(userId, roomId)
   usePlayerSocketListeners(roomId)
   useVoteSocketListeners(roomId)
+  useRoomMemberSocketListeners(roomId, userId)
 
   return null
 }
