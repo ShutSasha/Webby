ece12f8d897c3a75ef103ee7ea684dba150b14ad
Artem <118220003+guntersteam@users.noreply.github.com>
2026-07-06 16:01:05 +0100

    feat: add go services in registry generation script, add ability for specifying single service to push

--- scripts/registry_generation.py
+++ scripts/registry_generation.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import argparse
 
 REGISTRY = "eu-frankfurt-1.ocir.io"
 NAMESPACE = "PASTE NAMESPACE"
@@ -10,12 +11,37 @@ SERVER_DIR = os.path.join(os.path.dirname(__file__), "..", "server")
 WEB_DIR = os.path.join(os.path.dirname(__file__), "..", "web")
 
 SERVICES = [
-    {"name": "webby-api-gateway","dockerfile": "Webby.ApiGetaway/Dockerfile","context": SERVER_DIR},
-    {"name": "webby-auth-service","dockerfile": "Webby.AuthService/Dockerfile","context": SERVER_DIR},
-    {"name": "webby-achievement-service","dockerfile": "Webby.AchievementService/Dockerfile","context": SERVER_DIR},
-    {"name": "webby-notification-service", "dockerfile": "Webby.NotificationService/Dockerfile","context": SERVER_DIR},
-    {"name": "webby-user-service","dockerfile": "Webby.UserService/Dockerfile","context": SERVER_DIR},
-    {"name": "webby-video-service","dockerfile": "Webby.VideoService/Dockerfile", "context": SERVER_DIR},
+    # ASP.NET CORE SERVICES
+    {"name": "webby-api-gateway",
+        "dockerfile": "Webby.ApiGetaway/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-auth-service",
+        "dockerfile": "Webby.AuthService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-achievement-service",
+        "dockerfile": "Webby.AchievementService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-notification-service",
+        "dockerfile": "Webby.NotificationService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-user-service",
+        "dockerfile": "Webby.UserService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-video-service",
+        "dockerfile": "Webby.VideoService/Dockerfile", "context": SERVER_DIR},
+
+    # GO SERVICES
+    {"name": "webby-admin-service",
+        "dockerfile": "Webby.AdminService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-chat-service",
+        "dockerfile": "Webby.ChatService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-room-category-service",
+        "dockerfile": "Webby.RoomCategoryService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-room-queue-service",
+        "dockerfile": "Webby.RoomQueueService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-room-service",
+        "dockerfile": "Webby.RoomService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-votes-service",
+        "dockerfile": "Webby.VotesService/Dockerfile", "context": SERVER_DIR},
+    {"name": "webby-ws-gateway",
+        "dockerfile": "Webby.WsGateway/Dockerfile", "context": SERVER_DIR},
+
+    # WEB
     {
         "name": "webby-web",
         "dockerfile": "./Dockerfile",
@@ -48,7 +74,6 @@ def build_and_push(service):
     print(f"SERVICE: {service['name']}")
     print(f"{'='*50}")
 
-
     build_cmd = ["docker", "build", "-t", image, "-f", dockerfile]
 
     if "build_args" in service:
@@ -63,14 +88,36 @@ def build_and_push(service):
 
 
 def main():
-    print(f"Registry: {REGISTRY}/{NAMESPACE}")
-    print(f"Сервисов: {len(SERVICES)}")
+    parser = argparse.ArgumentParser(
+        description="Script for pushing docker images into OCI registry")
+    parser.add_argument(
+        '-s', '--service',
+        type=str,
+        help="Name of the image (e.g, webby-user-service). Missed param build and push all services"
+    )
+    args = parser.parse_args()
 
-    for service in SERVICES:
+    services_to_process = SERVICES
+    if args.service:
+        services_to_process = [
+            s for s in SERVICES if s['name'] == args.service]
+
+        if not services_to_process:
+            print(
+                f"Error: Service with name '{args.service}' wasn't found in the list.")
+            print("Available services:")
+            for s in SERVICES:
+                print(f"  - {s['name']}")
+            sys.exit(1)
+
+    print(f"Registry: {REGISTRY}/{NAMESPACE}")
+    print(f"Services for processing: {len(services_to_process)}")
+
+    for service in services_to_process:
         build_and_push(service)
 
     print(f"\n{'='*50}")
-    print("✓ All docker images collected and uploaded")
+    print("✓ All selected docker images collected and uploaded")
 
 
 if __name__ == "__main__":
